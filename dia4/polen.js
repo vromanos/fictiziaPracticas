275bd05c87ea9c334acdb0d3629d9b915fb8cc5c
vromanos <37002940+vromanos@users.noreply.github.com>
2018-11-30 13:57:12 +0100

    Add files via upload

---
+++ dia4/polen.js
@@ -0,0 +1,28 @@
+// Sacar en el html los datos de polen (http://airemad.com/api/v1/pollen)
+
+function request(url, fnCallback) {
+
+	fetch(url)
+	    .then(function(response) {
+	    	return response.json();
+	    })
+	    .then(function(data) {
+	      	fnCallback(data); 
+	    })
+	    .catch(function(error) {
+	      	console.log("Error en Fetch => " + error)
+	    });  
+};
+
+var madrid = "Madrid";
+var cadenaHtml = "";
+request("http://api.openweathermap.org/data/2.5/weather?q="+madrid+"&APPID=575d6a8a0eda57506aefa0a327af4b19", data => {
+	console.log(data);
+	var div = document.querySelector("#resultado");
+	cadenaHtml += "Estacion " + data.name + "<ul>";
+	data.weather.forEach(wea => {
+		cadenaHtml += "<li>"+ wea.description +"</li>";
+	})
+	cadenaHtml += "</ul>";
+	div.innerHTML = cadenaHtml;
+});
