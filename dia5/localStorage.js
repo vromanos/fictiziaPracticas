e2e828246c9a94539925fb3a1c8b590679b11039
vromanos <37002940+vromanos@users.noreply.github.com>
2018-12-03 00:25:59 +0100

    Add files via upload
    Ejercicios LocalStorage

---
+++ dia5/localStorage.js
@@ -0,0 +1,65 @@
+
+function actualizarListaContactos() {
+	var listaContactos = "";
+	const localStorage = window.localStorage;
+	var contadorActual = 1;
+	for ( var i = 0; i < localStorage.length; i++ ) {
+		if (localStorage.key(i).startsWith("contacto")) {
+			var contacto = localStorage.getItem(localStorage.key(i)).split("##");
+			listaContactos += `<li><div>Nombre = ${contacto[0]} Telefono = ${contacto[1]} Email = ${contacto[2]}</div><input type="button" onClick="eliminarContacto('${localStorage.key(i)}')" value="Eliminar"></li>`;
+		}
+		contadorActual++;
+	}
+	document.querySelector("#listaContactos").innerHTML = listaContactos;
+	contadorContactos = contadorActual;
+}
+
+function eliminarContacto(clave) {
+	window.localStorage.removeItem(clave);
+	actualizarListaContactos();
+}
+
+var contadorContactos = 1;
+var Contacto = function (nombre, telefono, email) {
+	this.nombre = nombre;
+	this.telefono = telefono;
+	this.email = email;
+	this.id = contadorContactos++;
+	this.guardarContacto = function () {
+		window.localStorage.setItem("contacto"+this.id, this.nombre+"##"+this.telefono+"##"+this.email);
+	}
+};
+
+(function (){
+	// Inicializacipn
+	if (window.localStorage) {
+	    console.info("La funcion Html5 localStorage esta soportada!");
+	} else {
+	    console.warn('Su navegador no soporta localStorage. Actualice su navegador');
+	}
+
+	document.querySelector("#guardarContacto").addEventListener('click', e => {
+		var contactoNombre = document.querySelector("#nombre").value;
+		var contactoTelefono = document.querySelector("#telefono").value;
+		var contactoEmail = document.querySelector("#email").value;
+		var miContacto = new Contacto(contactoNombre, contactoTelefono, contactoEmail);
+		miContacto.guardarContacto();
+		document.querySelector("#nombre").value = "";
+		document.querySelector("#telefono").value = "";
+		document.querySelector("#email").value = "";
+		actualizarListaContactos();
+	});
+
+	document.querySelector("#borrarContactos").addEventListener('click', e => {
+		if (confirm("¿Seguro que desea borrar todos los contactos?")) {
+			for ( var i = 0; i < localStorage.length; i++ ) {
+				if (localStorage.key(i).startsWith("contacto")) {
+					localStorage.removeItem(localStorage.key(i));
+				}
+			}
+			actualizarListaContactos();
+		}
+	})
+
+	actualizarListaContactos();
+})();
